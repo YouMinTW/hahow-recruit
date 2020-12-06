e22757b77102ea55764904ffaa3debe5cc9c3410
YouMinTW <m791213p@gmail.com>
2020-12-06 12:49:20 +0800

    [REFACTOR] enhance Not Found Page flow control semantics readability.

--- src/Routes.js
+++ src/Routes.js
@@ -27,7 +27,7 @@ const Routes = () => (
           )
         }}
       />
-
+      {/* Client 搜尋不到的路由，類似於後端回傳 404，但不傳遞 error prop （預設行為可以返回上一頁） */}
       <Route component={() => <NotFoundPage message='Not Found' />} />
     </Switch>
   </BrowserRouter>
